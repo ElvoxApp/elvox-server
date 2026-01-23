7e3a56dddbbd73c1c58fe38cee45c9ee43b7adc8
Jiyad <mdjiyade@gmail.com>
2026-01-23 18:57:37 +0530

    Updated error handler to include error code

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,7 +1,8 @@
 const errorHandler = (err, req, res, next) => {
     const status = err.status || 500
     res.status(status).json({
-        error: err.message || "Internal Server Error"
+        error: err.message || "Internal Server Error",
+        code: err.code || null
     })
 }
 
