ace0a2756b55e9b71e35360bcf5c29c102087ca4
Jiyad <mdjiyade@gmail.com>
2026-01-24 10:57:47 +0530

    Moved logs to election route

--- routes/web/electionRoute.js
+++ routes/web/electionRoute.js
@@ -14,39 +14,54 @@ import {
     generateSecretKey,
     streamEvents
 } from "../../controllers/electionController.js"
+import { getLogs } from "../../controllers/logController.js"
 import requireRole from "../../middleware/requireRole.js"
 import requirePassword from "../../middleware/requirePassword.js"
 
 const router = Router()
 
 router.get("/", getElection)
+
 router.get("/all", getAllElections)
+
 router.post("/", requireRole(["admin"]), createElection)
+
 router.get("/:id/supervisors", requireRole(["admin"]), getSupervisors)
+
 router.post("/:id/supervisors", requireRole(["admin"]), updateSupervisors)
+
 router.get(
     "/:id/category-config",
     requireRole(["admin", "student"]),
     getReservedClasses
 )
+
 router.patch(
     "/:id/category-config",
     requireRole(["admin"]),
     updateReservedClasses
 )
+
 router.patch(
     "/:id/auto-publish",
     requireRole(["admin"]),
     updateAutoPublishResults
 )
+
 router.post("/:id/secret-key", requireRole(["admin"]), generateSecretKey)
+
+router.get("/:id/logs", requireRole(["admin"]), getLogs)
+
 router.get(
     "/:id/events/stream",
     requireRole(["admin", "supervisor"]),
     streamEvents
 )
+
 router.delete("/:id", requireRole(["admin"]), requirePassword, deleteElection)
+
 router.patch("/:id", requireRole(["admin"]), updateElection)
+
 router.get("/:id", getElectionDetails)
 
 export default router
--- routes/web/logRoute.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express"
-import { getLogs } from "../../controllers/logController.js"
-
-const router = new Router()
-
-router.get("/:id/logs", getLogs)
-
-export default router
