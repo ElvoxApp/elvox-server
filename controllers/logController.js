d75425d67ce7021d75c4d9f4c50f538302a3c4f8
Jiyad <mdjiyade@gmail.com>
2026-01-24 10:47:15 +0530

    Removed live logs from logs route

--- controllers/logController.js
+++ controllers/logController.js
@@ -1,7 +1,4 @@
-import pool from "../db/db.js"
 import * as logService from "../services/logService.js"
-import CustomError from "../utils/CustomError.js"
-import { addClient, emitLog, removeClient } from "../utils/sseManager.js"
 
 export const getLogs = async (req, res, next) => {
     try {
@@ -11,37 +8,4 @@ export const getLogs = async (req, res, next) => {
     } catch (err) {
         next(err)
     }
-}
-
-export const streamLogs = async (req, res, next) => {
-    const electionId = req.params.id
-
-    try {
-        const electionRes = await pool.query(
-            "SELECT status FROM elections WHERE id = $1 LIMIT 1",
-            [electionId]
-        )
-
-        if (electionRes.rowCount === 0)
-            throw new CustomError("No election found with the given id", 404)
-
-        if (electionRes.rows[0].status === "closed")
-            throw new CustomError(
-                "Live logs are only available for active elections",
-                409
-            )
-    } catch (err) {
-        return next(err)
-    }
-
-    res.setHeader("Content-Type", "text/event-stream")
-    res.setHeader("Cache-Control", "no-cache")
-    res.setHeader("Connection", "keep-alive")
-    res.flushHeaders()
-
-    addClient(electionId, res)
-
-    req.on("close", () => {
-        removeClient(electionId, res)
-    })
 }
--- routes/web/logRoute.js
+++ routes/web/logRoute.js
@@ -1,9 +1,8 @@
 import { Router } from "express"
-import { getLogs, streamLogs } from "../../controllers/logController.js"
+import { getLogs } from "../../controllers/logController.js"
 
 const router = new Router()
 
-router.get("/:id/logs/stream", streamLogs)
 router.get("/:id/logs", getLogs)
 
 export default router
